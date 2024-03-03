f8121ef50f261f09209803bc4a1df77e06f0ec9a
jyhyun1008 <hyun1008@kasi.re.kr>
2024-03-03 20:24:24 +0900

    first commit

---
+++ page/index.md
@@ -0,0 +1,7 @@
+> 이치고아메
+
+![main.jpg](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKwlV2phrYKT7R8pMmkB5__AGTcxavNMVjgXiYsdcnvTm8VsAr81zR5pES6Vipt7KPRnE&usqp=CAU)
+
+# 소개
+
+코바늘로 구체관절인형을 만들어요.
---
+++ settings.js
@@ -0,0 +1,13 @@
+// 기본정보
+const SITE_URL = "https://hgjs.moe"
+const SITE_TITLE = "현가지성(HGJS)"
+const SITE_DESCRIPTION = "여고생으로 전직한 공자의 제자들이 동아리에서 밴드를 해 보았습니다"
+const SITE_FOOTER_HTML = "Copyright 2024. 현가지성(HGJS). All rights reserved."
+
+// 테마
+const ACCENT_COLOR = "#28d79c"
+const BG_COLOR = "#eee"
+
+// 깃허브 정보
+const USERNAME = "jyhyun1008"
+const REPOSITORY = "hgjsmoe"
